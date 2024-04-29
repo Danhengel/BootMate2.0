c12a3eaafbad9ad962f58e4996cb35a850027a88
Danhengel <d.hengel@yahoo.com>
2024-04-29 06:27:56 -0400

    Upfate

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,7 +6,9 @@ import { useMutation, useQuery } from "@apollo/client";
 import { LOGIN } from "../utils/mutations";
 import { QUERY_STUDENTS } from "../utils/queries";
 import UserCard from "../components/Card/UserCard";
-import { Box, Button, FormControl, FormLabel, Input, VStack, Heading, Text, Image, Link as ChakraLink } from "@chakra-ui/react";
+import { Heading, Box, Button, FormControl, FormLabel, Input, VStack, Text, Image as ChakraImage, Link as ChakraLink } from "@chakra-ui/react";
+import bgImage from '../assets/splatted-green-blue-orange-color-black-background.jpg';
+import bootmateLogo from '../assets/bootmate-logo.png';
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -34,37 +36,37 @@ function Login(props) {
   };
 
   return (
-    <Box className="popup">
-      <Box className="popup-inner">
-        <Heading as="h2">Login</Heading>
-        <form onSubmit={handleFormSubmit}>
-          <VStack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email:</FormLabel>
-              <Input
-                placeholder="Email"
-                name="email"
-                type="email"
-                id="email"
-                onChange={handleChange}
-              />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password:</FormLabel>
-              <Input
-                placeholder="Password"
-                name="password"
-                type="password"
-                id="pwd"
-                onChange={handleChange}
-              />
-            </FormControl>
-            <Button type="submit" onClick={Login} colorScheme="teal">
-              Login
-            </Button>
-          </VStack>
-        </form>
-        <Button onClick={props.toggle} mt={4} colorScheme="teal">Close</Button>
+    <Box className="container" style={{ backgroundImage: `url(${bgImage})` }}>
+      <Box className="popup">
+        <Box className="popup-inner">
+          <Heading as="h2">Login</Heading>
+          <form onSubmit={handleFormSubmit}>
+            <VStack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email:</FormLabel>
+                <Input
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password:</FormLabel>
+                <Input
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <Button type="submit" colorScheme="teal">
+                Login
+              </Button>
+            </VStack>
+          </form>
+          <Button onClick={props.toggle} mt={4} colorScheme="teal">Close</Button>
+        </Box>
       </Box>
     </Box>
   );
@@ -74,7 +76,8 @@ export default function HomePage() {
   const { loading, data } = useQuery(QUERY_STUDENTS);
   const students = data?.students || [];
 
-  const searchAll = () => {
+  const searchAll = (event) => {
+    event.preventDefault();
     return (
       <Box>
         {loading ? <Box>Loading...</Box> : <UserCard students={students} />}
@@ -83,50 +86,34 @@ export default function HomePage() {
   };
 
   const [seen, setSeen] = useState(false);
-  function togglePop() {
+  const togglePop = () => {
     setSeen(!seen);
-  }
+  };
 
-    return (
+  return (
+    <Box>
+      <ChakraImage src={bgImage} alt="background image" />
       <Box as="section" id="home-page">
         {Auth.loggedIn() ? (
           <>
             <NavBar />
-            <Box className="container">
-              <Image src="./assets/bootmate_logo.png" alt="bootMate() logo" />
-              <Heading as="h1">What are you waiting for?</Heading>
-              <Heading as="h2">Search for projects to join or create your own!</Heading>
-    
-              <Box className="search-container">
-                <form onSubmit={searchAll}>
-                  <FormControl id="search">
-                    <Input type="text" placeholder="Search..." name="search" />
-                  </FormControl>
-                  <Button type="submit" id="search" className="btn" colorScheme="teal">
-                    Search
-                  </Button>
-                </form>
-              </Box>
-    
-              <Box className="search-results">{searchAll()}</Box>
+            <Box className="search-container">
+              <form onSubmit={searchAll}>
+                <FormControl id="search">
+                  <Input type="text" placeholder="Search..." name="search" />
+                </FormControl>
+                <Button type="submit" id="search" className="btn" colorScheme="teal">
+                  Search
+                </Button>
+              </form>
             </Box>
+            <Box className="search-results">{searchAll()}</Box>
           </>
         ) : (
           <Box className="container">
-            <Heading as="h1">Welcome to bootMate()</Heading>
-    
-            <Heading as="h2" id="subtag">Connect. Collaborate. Create</Heading>
-            <Box className="text">
-              <Text id="description">
-                bootMate() is a platform for developers to connect and collaborate
-                on projects.
-                <br></br>
-                <br></br>
-                Connect, Collaborate, Code
-              </Text>
-              <Heading as="h4" id="subtext">Login or Sign up today to get started!</Heading>
-            </Box>
-    
+            <Box className="logo">
+              <ChakraImage src={bootmateLogo} alt="bootMate logo" />
+            
             <Box className="login">
               <Button className="btn" id="login" onClick={togglePop} colorScheme="teal">
                 Login
@@ -137,9 +124,11 @@ export default function HomePage() {
                   Sign Up
                 </Button>
               </ChakraLink>
+              </Box>
             </Box>
           </Box>
         )}
       </Box>
-      );
-    }
+    </Box>
+  );
+}
